c5ee09e47f4c1f54341575c0c616b7fb35780fac
Jenny Pollack <JennyPollack@Jennys-MacBook-Pro.local>
2015-11-06 11:28:31 -0800

    add code for unique

--- test/array_tests.js
+++ test/array_tests.js
@@ -10,7 +10,7 @@ var shifter = require(__dirname + '/../lib/shift');
 describe('push function', function(){
 	it('should add an item to the end of the array', function(){
 		var testPush = [1, 2, 3]; 
-		expect(pusher(testPush, 4)).to.eql([1, 2, 3, 4]);
+		expect(pusher(testPush, 4)).to.eql([1, 2, 3, 4]); 
 	});
 
 });
@@ -23,17 +23,25 @@ describe('pop function', function(){
 });
 
 describe('shift function', function(){
-	it('should shift', function(){
+	it('should remove and return the first element of the array', function(){
 		var testShift = [1, 2, 3];
-		expect(shifter(testShift)).to.eql(true); 
+		expect(shifter(testShift)).to.eql(1); 
+		expect(testShift.length).to.eql(2); 
 	});
 });
 
 describe('unshift function', function(){
-	it('should ', function(){
+	it('should add an element to the beginning of the array', function(){
 		var testUnshift = [1, 2, 3]; 
 		expect(unshifter(testUnshift, 4)).to.eql([4, 1, 2, 3]);
+		expect(testUnshift.length).to.eql(4); 
 	});
 });
 
+describe('unique function', function(){
+	it('should return an array without duplicates', function(){
+		var testUnique = [1,1,2,2,3];
+		expect(unique(testUnique)).to.eql([1,2,3]); 
+	});
+});
 
